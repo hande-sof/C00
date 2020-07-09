0e98250b1f1db9ce9bd3fdb4c2ce8620e8b2fd43
soilee <hande.sof@gmail.com>
2020-07-09 22:35:02 +0900

    ex07

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,8 +6,27 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 16:33:23 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/09 16:33:48 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/09 22:34:35 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putnbr(int nb);
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	char c;
+
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	else if (nb >= 0 && nb < 10)
+	{
+		c = nb + '0';
+		write(1, &c, 1);
+		return ;
+	}
+	ft_putnbr(nb / 10);
+	ft_putnbr(nb % 10);
+}
