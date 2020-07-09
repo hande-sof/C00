81857ca11a0b8b005e1e8f42910db6cf4103bde0
soilee <hande.sof@gmail.com>
2020-07-09 16:31:28 +0900

    ex06

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,57 +6,43 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 16:27:33 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/07 22:03:46 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/09 16:31:07 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	c[5];
-
-void	last_num(void)
+void	print(int num)
 {
-	while (c[3] >= '9')
-	{
-		while (c[4] >= '9')
-		{
-			if (c[3] == '9' && c[4] == '9')
-			{
-				if(c[1] == '9')
-				{
-					c[0]++;
-				}
-				c[1]++;
-				c[4] = c[1] + 1;
-			}
-			write(1, &c, 5);
-			c[4]++;
-		}
-		c[3]++;
-	}
+	char n[2];
+	
+	n[0] = num / 10 + '0';
+	n[1] = num % 10 + '0';
+	write(1, &n, 2);
 }
-
- void    first_num(void)
- {
-	 while (c[0] >= '9')
-	 {
-		 while (c[1] >= '9')
-		 {
-			 last_num();
-			 c[1]++;
-		 }
-		 c[0]++;
-	 }
- }
 
 void	ft_print_comb2(void)
 {
-	char c[4];
+	int nums[2];
+	char c[2];
 
-	c[0] = '0';
-	c[1] = '0';
-	c[2] = ' ';
-	c[3] = '0';
-	c[4] = '1';
-	first_num();
+	nums[0] = 0;
+	c[0] = ',';
+	c[1] = ' ';
+	while (nums[0] <= 98)
+	{
+		nums[1] = nums[0] + 1;
+		while (nums[1] <= 99)
+		{
+			print(nums[0]);
+			write(1, &c[1], 1);
+			print(nums[1]);
+			if (!(nums[0] == 98 && nums[1] == 99))
+			{
+				write(1, &c, 2);
+			}
+			nums[1]++;
+		}
+		nums[0]++;
+	}
 }
