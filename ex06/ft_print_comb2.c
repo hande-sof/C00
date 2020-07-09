46b8e9b6ed2d5ad98875fc043728c7d420845daa
soilee <hande.sof@gmail.com>
2020-07-09 16:43:42 +0900

    ex07 start

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: soilee <soilee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/07 16:27:33 by soilee            #+#    #+#             */
-/*   Updated: 2020/07/09 16:31:07 by soilee           ###   ########.fr       */
+/*   Updated: 2020/07/09 16:42:01 by soilee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	print(int num)
 {
 	char n[2];
-	
+
 	n[0] = num / 10 + '0';
 	n[1] = num % 10 + '0';
 	write(1, &n, 2);
@@ -23,23 +23,20 @@ void	print(int num)
 
 void	ft_print_comb2(void)
 {
-	int nums[2];
-	char c[2];
+	int		nums[2];
 
 	nums[0] = 0;
-	c[0] = ',';
-	c[1] = ' ';
 	while (nums[0] <= 98)
 	{
 		nums[1] = nums[0] + 1;
 		while (nums[1] <= 99)
 		{
 			print(nums[0]);
-			write(1, &c[1], 1);
+			write(1, " ", 1);
 			print(nums[1]);
 			if (!(nums[0] == 98 && nums[1] == 99))
 			{
-				write(1, &c, 2);
+				write(1, ", ", 2);
 			}
 			nums[1]++;
 		}
